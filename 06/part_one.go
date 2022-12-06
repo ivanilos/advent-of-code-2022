711b565278930ab1577b3f13137b0b6592df4935
ivanilos <ivanilos@gmail.com>
2022-12-06 02:13:17 -0300

    Day 6 solutions

---
+++ 06/part_one.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func createFileScanner(fileName string) (*bufio.Scanner, *os.File) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("unable to read file: %v", err)
+	}
+
+	reader := bufio.NewScanner(file)
+	return reader, file
+}
+
+func parseInput() string {
+	scanner, file := createFileScanner("input.txt")
+	defer file.Close()
+
+	scanner.Scan()
+
+	return scanner.Text()
+}
+
+func unique(s string) bool {
+	chars := map[rune]bool{}
+
+	for _, char := range s {
+		chars[char] = true
+	}
+	return len(chars) == len(s)
+}
+
+func solve(input string) int {
+	const needDiff = 4
+
+	for i, j := 0, needDiff-1; j < len(input); i, j = i+1, j+1 {
+		if unique(input[i : j+1]) {
+			return j + 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	input := parseInput()
+	result := solve(input)
+
+	fmt.Println(result)
+}
